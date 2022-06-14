7118e9aaa562c87ab62276015e507f897f5eacbd
barkin.narin <barkin.narin@trendyol.com>
2022-06-14 20:55:48 +0300

    new service added

--- coupon-type-service/src/main/java/com/order/orderservice/application/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.order.orderservice.application;
-
-import com.order.orderservice.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- coupon-type-service/src/main/java/com/order/orderservice/domain/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.order.orderservice.domain;
-
-public enum OrderStatus {
-
-    PENDING, RECEIVED, CANCELED
-}
